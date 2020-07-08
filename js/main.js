02e205ead686f5ab34141628c0b87f430f670602
Steven <steven1126@swbell.net>
2020-07-08 18:37:13 -0500

    Pulled gifs to website

--- js/main.js
+++ js/main.js
@@ -4,8 +4,17 @@ function generateCode () {
                 //variable made from quantity
                 var num = document.getElementById("quantity").value;
                 //giphy request
-                giphyRequest = "https://api.giphy.com/v1/gifs/search?q="+search+"&api_key=8zUdFzYDxUUXGaP5ydLmU0xLydgX2Vie&limit="+num;
-                $('#display').text(giphyRequest);
-                alert(giphyRequest);
-                // alert('Did it work?');
+                var giphyCode = "https://api.giphy.com/v1/gifs/search?q="+search+"&api_key=8zUdFzYDxUUXGaP5ydLmU0xLydgX2Vie&limit="+num;
+                $('#display').text(giphyCode);
+                var giphyRequest = $.get(giphyCode);
+                
+                //giphy code
+                giphyRequest.done(function(response) {
+                    console.log("Success! Got data!!", response);
+                    var pics = response.data
+                    for (i in pics) {
+                        $('.inner').append("<img src="+pics[i].images.original.url+"/>");
+                    };
+                });
             };
+
