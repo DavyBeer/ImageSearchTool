347c3b00e19a59ecab20c51b48a8d54a74eac461
Steven <steven1126@swbell.net>
2020-07-09 16:24:26 -0500

    Remove debug code

--- js/main.js
+++ js/main.js
@@ -6,7 +6,6 @@ function generateCode () {
     var num = document.getElementById("quantity").value;
     //giphy request
     var giphyCode = "https://api.giphy.com/v1/gifs/search?q="+search+"&api_key=8zUdFzYDxUUXGaP5ydLmU0xLydgX2Vie&limit="+num;
-    $('#display').text(giphyCode);
     var giphyRequest = $.get(giphyCode);
                 
     //giphy code
