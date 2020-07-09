b120402d832f9c3f5a812c28b350b84d20e22c75
Steven <steven1126@swbell.net>
2020-07-09 07:54:43 -0500

    Remove old images

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 function generateCode () {
-                //variable made from query
-                var search = document.getElementById("query").value;
-                //variable made from quantity
-                var num = document.getElementById("quantity").value;
-                //giphy request
-                var giphyCode = "https://api.giphy.com/v1/gifs/search?q="+search+"&api_key=8zUdFzYDxUUXGaP5ydLmU0xLydgX2Vie&limit="+num;
-                $('#display').text(giphyCode);
-                var giphyRequest = $.get(giphyCode);
+    $('#inner').empty();
+    //variable made from query
+    var search = document.getElementById("query").value;
+    //variable made from quantity
+    var num = document.getElementById("quantity").value;
+    //giphy request
+    var giphyCode = "https://api.giphy.com/v1/gifs/search?q="+search+"&api_key=8zUdFzYDxUUXGaP5ydLmU0xLydgX2Vie&limit="+num;
+    $('#display').text(giphyCode);
+    var giphyRequest = $.get(giphyCode);
                 
-                //giphy code
-                giphyRequest.done(function(response) {
-                    console.log("Success! Got data!!", response);
-                    var pics = response.data
-                    for (i in pics) {
-                        $('.inner').append("<img src="+pics[i].images.original.url+"/>");
-                    };
-                });
-            };
+    //giphy code
+    giphyRequest.done(function(response) {
+        console.log("Success! Got data!!", response);
+        var pics = response.data
+        for (i in pics) {
+            $('.inner').append("<img src="+pics[i].images.original.url+"/>");
+        };
+    });
+};
 
